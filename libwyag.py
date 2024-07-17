8d3c18685667f8560d1c85d0a282fde3e2c8bfd5
bivsbon <thanhminh20002005@gmail.com>
2024-07-17 17:57:32 +0700

    initial

---
+++ libwyag.py
@@ -0,0 +1,22 @@
+import argparse
+import collections
+import configparser
+
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="I am argparser")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add": cmd_add(args)
